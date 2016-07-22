5b7acc483b9ad27d112ab672a1fd8be30e76c01e
Nick Spinale <spinalen@carleton.edu>
2016-07-22 01:50:44 -0400

    problems with mtl

--- Graphics/XHB/Monad.hs
+++ Graphics/XHB/Monad.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Graphics.XHB.Monad
     ( X
     , unX
+
+    -- , asksX
+
     , req
     , reqAsync
     , notify
+
     , IOU(..)
     , (<$>>)
     , (<*>>)
@@ -12,24 +24,77 @@ module Graphics.XHB.Monad
 
 import Graphics.XHB
 import Graphics.XHB.Requests
+
+import Data.Constraint
+import Data.Typeable
 import Control.Monad.Except
 import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 
 
 newtype X m a = X { runX :: ReaderT Connection (ExceptT SomeError m) a }
-
-unX :: MonadIO m => X m a -> Connection -> m (Either SomeError a)
-unX = fmap runExceptT . runReaderT . runX
+  deriving (Functor, Applicative, Monad, MonadIO, Typeable)
 
 instance MonadTrans X where
+    lift = X . lift . lift
 
-instance Functor m => Functor (X m) where
+instance MonadError e m => MonadError e (X m) where
+    throwError = lift . throwError
+    catchError x f = toX $ \conn -> catchError (unX x conn) (flip ($) conn . unX . f) 
 
-instance Applicative m => Applicative (X m) where
+instance MonadReader r m => MonadReader r (X m) where
+    ask = lift ask
+    local f x = toX $ \conn -> local f (unX x conn)
 
-instance Monad m => Monad (X m) where
+instance MonadState s m => MonadState s (X m) where
+    get = lift get
+    put = lift . put
+
+instance MonadWriter w m => MonadWriter w (X m) where
+    tell = lift . tell
+    listen = X . listen . runX
+    pass = X . pass . runX
+
+
+-- Class --
+
+
+-- class Monad m => MonadX m where
+--     liftX :: X n a -> m a
+--     askX :: m Connection
+--     catchErrorX :: m a -> (SomeError -> m a) -> m a
+
+-- instance Monad m => MonadX (X m) where
+--     liftX = 
+--     askX = X $ ask
+--     catchErrorX x f = X $ catchError (runX x) (runX . f)
+
+-- -- TODO constraints
+-- instance ( MonadX m
+--          , MonadTrans t
+--          , Monad (t m)
+--          , MonadError SomeError (t (ReaderT Connection (ExceptT SomeError m)))
+--          ) => MonadX (t m) where
+-- -- instance forall m t n e. (MonadX m, MonadTrans t, Monad (t m), MonadError e n, MonadError e (t n)) => MonadX (t m) where
+--     askX = lift askX
+--     catchErrorX x f = catchError (lift $ runX x) (lift . runX . f)
+
+
+-- Convenience --
+
+
+toX :: (Connection -> m (Either SomeError a)) -> X m a
+toX = X . ReaderT . fmap ExceptT
+
+
+unX :: X m a -> Connection -> m (Either SomeError a)
+unX = fmap runExceptT . runReaderT . runX
+
+
+-- Communication --
 
 
 req :: (RequestWithReply a b, MonadIO m) => a -> X m b
@@ -40,6 +105,9 @@ reqAsync a = X . ReaderT $ \conn -> liftIO . fmap (X . lift . ExceptT . liftIO) 
 
 notify :: (Request a, MonadIO m) => a -> X m ()
 notify a = X . ReaderT $ \conn -> liftIO (request conn a)
+
+
+-- IOU --
 
 
 newtype IOU m a = IOU { runIOU :: m (m a) }
--- default.nix
+++ default.nix
@@ -1,9 +1,12 @@
-{ mkDerivation, base, mtl, stdenv, transformers, xhb, xhb-requests
+{ mkDerivation, base, constraints, mtl, stdenv, transformers, xhb
+, xhb-requests
 }:
 mkDerivation {
   pname = "xhb-monad";
   version = "0.1";
   src = ./.;
-  libraryHaskellDepends = [ base mtl transformers xhb xhb-requests ];
+  libraryHaskellDepends = [
+    base constraints mtl transformers xhb xhb-requests
+  ];
   license = stdenv.lib.licenses.mit;
 }
